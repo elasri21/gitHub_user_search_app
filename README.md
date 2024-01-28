c198114113113fbe6b0c95f5753c1eb1a1de9e93
elasri21 <islame.edu@gmail.com>
2024-01-28 10:57:26 +0100

    add files

---
+++ README.md
@@ -0,0 +1,15 @@
+# GitHub user search app
+
+## Discreption:
+
+- In this project, you'll use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.
+
+### Challenge From: [forontend mentor]
+
+### Tools:
+
+- [html, csss, js]
+
+### Coded By:
+
+- [elasri21('https://github.com/elasri21')]
---
+++ main.js
@@ -0,0 +1,54 @@
+const url = 'https://api.github.com/users'
+
+
+
+
+const btn = document.querySelector(".btn");
+const field = document.querySelector(".field");
+
+btn.addEventListener("click", async function getData() {
+    const username = field.value.trim();
+    const user = `${url}/${username}`
+    const res = await fetch(user);
+
+    const data = await res.json();
+
+    console.log(data);
+
+    const avatar  = document.querySelector(".avatar img")
+    avatar.src = data['avatar_url']
+    
+    const userName = document.querySelector(".top-section h2")
+    userName.innerHTML = data['login']
+    
+    const lightName = document.querySelector(".top-section p")
+    lightName.innerHTML = data['name']
+    
+    const joinedDate = document.querySelector(".joined-date");
+    joinedDate.innerHTML = data['created_at'];
+    
+    const bio = document.querySelector(".descreption p")
+    bio.innerHTML = data['bio'] != null ? data['bio'] : 'This profile has no bio';
+    
+    const repos = document.querySelector(".repos h4")
+    repos.innerHTML = data['public_repos']
+    
+    const followers = document.querySelector(".followers h4")
+    followers.innerHTML = data['followers']
+    
+    const following = document.querySelector(".following h4")
+    following.innerHTML = data['following']
+    
+    const located = document.querySelector(".location span");
+    located.innerHTML = data['location'] != null ? data['location'] : "Somewhere on earth";
+
+    const twitter = document.querySelector(".twitter span");
+    twitter.innerHTML = data['twitter_username'] != null ? data['twitter_username'] : 'Not available'
+    
+    const blog = document.querySelector(".blog span");
+    blog.innerHTML = data['blog'] != '' ? data['blog'] : "Has no blog!";
+
+    const sgit = document.querySelector(".sgit span");
+    sgit.innerHTML = data['login']
+});
+
